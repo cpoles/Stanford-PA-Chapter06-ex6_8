297df9efce0672bb2183e9f223f9a1f4e2063580
cpoles <carlos.poles@griffithuni.edu.au>
2018-06-06 10:55:12 +1000

    Initial Commit

---
+++ ex6_8/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ex6_8
+//
+//  Created by Carlos Poles on 6/6/18.
+//  Copyright © 2018 Carlos Poles. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
